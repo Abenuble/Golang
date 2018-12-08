0f5517e208e08c528d6d1f968901383023702fd4
Abenuble <abee.abdee@gmail.com>
2018-12-08 22:19:06 +0700

    part 7

--- gotut.go
+++ gotut.go
@@ -2,18 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Whoa, Go is near!")
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
+type car struct {
+	gas_pedal      uint16 // min 0 max 65535
+	brake_pedal    uint16
+	steering_wheel int16 // -32k - +32k
+	top_speed_kmh  float64
 }
-func about_handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Expert web design by abenuble!")
+
+func (c car) kmh() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
+}
+func (c car) mph() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax / kmh_multiple)
 }
 
 func main() {
-	http.HandleFunc("/", index_handler)
-	http.HandleFunc("/about/", about_handler)
-	http.ListenAndServe(":8000", nil)
+	a_car := car{
+		gas_pedal:      65535,
+		brake_pedal:    0,
+		steering_wheel: 12561,
+		top_speed_kmh:  225.0,
+	}
+
+	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
 }
